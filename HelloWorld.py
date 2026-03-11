b6cde3eb472c5f633be9eb7984cd21a4f1c2987e
Alexander <pchp250g8@tutamail.com>
2026-03-11 16:11:42 +0300

    Modified Source Code

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,6 +3,7 @@ import curses
 scr = curses.initscr()
 curses.curs_set(0)
 scr.clear() 
-scr.addstr(10,10,"Hello,world!!!")
+scr.addstr(10, 10, "Hello,world!!!")
+scr.addstr(11, 10, "Curses Library")
 scr.getch()
 curses.endwin()
